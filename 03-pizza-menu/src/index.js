aac0126a96b1f567e97c9aa13bc56273236de2d2
leonamagaya <leonamagaya@gmail.com>
2024-02-26 12:15:59 +0100

    extracting jsx into a new component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -131,12 +131,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We are Open until {closeHour}:00. Come visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -144,8 +139,19 @@ function Footer() {
       )}
     </footer>
   );
-  //return React.createElement("footer", null, "We're curremtly Open");
 }
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We are Open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+//return React.createElement("footer", null, "We're curremtly Open");
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
