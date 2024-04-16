8a827b987a8db794caad91063623545d05d43d18
leonamagaya <leonamagaya@gmail.com>
2024-04-16 14:35:30 +0200

    Revert "how react works behind the scenes"
    This reverts commit 69d238f825e463d3dde4852bbdd69310a06fb410.

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -8,10 +8,6 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
